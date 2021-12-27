dea4d27941f030e6d10d4b9057537483ee20bd36
Xiphiar <me@xiphiar.com>
2021-12-27 12:27:37 -0500

    add twitter announcements

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,21 @@ import * as dotenv from 'dotenv';
 // @ts-ignore
 import storage from 'node-persist';
 import { CosmWasmClient } from 'secretjs';
+import { TwitterApi } from 'twitter-api-v2';
+import Axios from 'axios';
+import fs from 'fs';
 dotenv.config();
+
+//const twitterClient = new TwitterApi('AAAAAAAAAAAAAAAAAAAAAJDWXQEAAAAA99xRi%2FrRT9gSBoCtx%2FgTN47dM8I%3D58mitqWWZBm6WxApRrDyI3X6F9ImLMJlkRkopcnpfmuKISIb05');
+
+console.log(process.env.TWITTER_APP_KEY, process.env.TWITTER_APP_SECRET, process.env.TWITTER_ACCESS_KEY, process.env.TWITTER_ACCESS_SECRET)
+const twitterClient = new TwitterApi({
+  // @ts-ignore
+  appKey: process.env.TWITTER_APP_KEY,
+  appSecret: process.env.TWITTER_APP_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_KEY,
+  accessSecret: process.env.TWITTER_ACCESS_SECRET,
+});
 
 // @ts-ignore
 const queryJs = new CosmWasmClient(process.env.REST_URL);
@@ -24,6 +38,19 @@ const query2 = {
 const aliens = [635,2890,3100,3443,5822,5905,6089,7523,7804];
 const apes = [372,5795,6915,5577,5217,8498,9280,1021,4464,6965,4178,8219,2491,6145,2924,4156,7191,2243,5314,2460,2140,2386,2711,9265];
 const zombies = [8386,3831,5336,4472,4513,6491,4874,9203,4830,6704,5573,9804,3636,8553,6297,9474,5299,1526,2566,2132,4850,2560,5489,8909,1190,1748,6586,2424,8127,1119,6784,7337,6649,7252,7660,8531,9955,8957,5742,9838,5944,2681,9997,5312,6275,5066,7756,2338,987,8857,2967,3211,7014,3393,7458,1886,7121,3493,5761,117,2249,7914,2938,5253,8307,7127,2306,9909,2066,1658,5234,8472,9368,6515,4747,1478,6304,8780,1935,1374,3489,2329,2484,3609,5412,2708,3328,4559];
+
+async function downloadImage(url: string, filepath: string) {
+  const response = await Axios({
+      url,
+      method: 'GET',
+      responseType: 'stream'
+  });
+  return new Promise((resolve, reject) => {
+      response.data.pipe(fs.createWriteStream(filepath))
+          .on('error', reject)
+          .once('close', () => resolve(filepath)); 
+  });
+}
 
 const exampleEmbed = new MessageEmbed()
 	.setColor('#0099ff')
@@ -59,6 +86,7 @@ function getColor(id: string): string{
 }
 
 async function intervalFunc(channel: any) {
+  //load last known sale height from persistent storage, will only announce sales after this height
   const lastKnownHeight = parseInt(await storage.getItem('lastKnownHeight') || 0);
   console.log("checking for sales from height ", lastKnownHeight)
 
@@ -67,7 +95,6 @@ async function intervalFunc(channel: any) {
   if (!lastKnownHeight || lastKnownHeight==0) {
     console.log("No persistent data, will not announce old sales. New height: ", saleInfo.collection_purchases.history[0].block_height)
     await storage.setItem('lastKnownHeight',saleInfo.collection_purchases.history[0].block_height)
-    //lastKnownHeight = saleInfo.collection_purchases.history[0].block_height;
     return;
   }
 
@@ -96,11 +123,20 @@ async function intervalFunc(channel: any) {
           )
           .setTimestamp()
         channel.send({ embeds: [punkEmbed] })
+
+        //download image for twitter
+        await downloadImage(listingInfo.listing_info.sale_item.already_minted_nft.nft_info.public_metadata.extension.image, "file2.png")
+
+        //create and send tweet
+        const mediaId = await twitterClient.v1.uploadMedia('./file2.png');
+        const twitMsg = `Secret Punk ${punkID} just sold for ${price} $SCRT on @StashhApp! #SecretPunks  https://stashh.io/asset/secret19syw637nl4rws0t9j5ku208wy8s2tvwqvyyhvu/${punkID}`
+        await twitterClient.v1.tweet(twitMsg,{ media_ids: mediaId });
       }
 
     }
   }
-  //lastKnownHeight = saleInfo.collection_purchases.history[0].block_height;
+
+  //save height of the latest sale to persistent storage
   await storage.setItem('lastKnownHeight',saleInfo.collection_purchases.history[0].block_height)
   console.log("Done, new height: ", saleInfo.collection_purchases.history[0].block_height)
 }
